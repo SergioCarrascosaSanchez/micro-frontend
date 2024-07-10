eb92dcb4123d02255b833be9bf177be363d8d4a5
SergioCarrascosaSanchez <sercarsa11@gmail.com>
2024-07-10 22:31:02 +0200

    adds shared chip component

--- react-host/src/App.tsx
+++ react-host/src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
+import "./index.css";
+
+// @ts-ignore
+import Chip from "react_remote/Chip";
 
 const App = () => (
   <div className="container">
     <div>Name: react-host</div>
+    <Chip label="Chip" color="primary" size="large" />
     <div>Framework: react</div>
     <div>Language: TypeScript</div>
     <div>CSS: Empty CSS</div>
   </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+);
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-root.render(<App />)
+root.render(<App />);
